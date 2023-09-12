a649923ff5d647dead11007ff43b1de5580f0312
jozzydgr8 <jozzydgr8@gmail.com>
2023-09-12 12:23:46 +0100

    finaledits

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-
-
     var date = new Date();
-    var options = {weekday: 'long'}
+    var options = {weekday: 'long'};
     var hours = date.getUTCHours();
     var minutes = date.getUTCMinutes();
     var seconds = date.getUTCSeconds();
     var mSecs = date.getUTCMilliseconds();
-    
-    var day = date.toLocaleDateString(undefined, options) ;
+    var day = date.toLocaleDateString(undefined, options);
 
 
-    var time = (`${mSecs}`)
+    var time = (`${mSecs}`);
     var dateSpan = document.getElementById("date");
     var timeSpan = document.getElementById("time");
     timeSpan.textContent = time;
